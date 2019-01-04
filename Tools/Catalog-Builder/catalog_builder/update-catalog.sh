26a615696b934687ea92de161a5e443062ecd118
Peter-Metz <peter.metz@aei.org>
2019-01-04 10:25:47 -0500

    catalog update script

---
+++ Tools/Catalog-Builder/catalog_builder/update-catalog.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export DATE=`date +%Y-%m-%d`
+export BRANCH_NAME=catalog-$DATE
+git checkout -b $BRANCH_NAME
+python catalog.py 
+git status
+git add -u
+git commit -m "Update catalog for date $DATE"
+git push origin catalog-$DATE
+git checkout test_automation
+git pull --no-edit origin $BRANCH_NAME
+git push origin test_automation
