cdca695eff554c218e9622891a477a082f3e2e98
Peter-Metz <pmetz@PMETZ-MBP.local>
2020-11-23 18:55:41 -0500

    convert initiatives page to markdown

--- Tools/Page-Builder/make_pages.sh
+++ Tools/Page-Builder/make_pages.sh
@@ -3,4 +3,5 @@
 pandoc -s -o ../../Community/contribute.html ../../Community/contribute.md -H head.html -B nav.html
 pandoc -s -o ../../Community/council.html ../../Community/council.md -H head.html -B nav.html
 pandoc -s -o ../../Community/roadmap.html ../../Community/roadmap.md -H head.html -B nav.html
+pandoc -s -o ../../Community/initiatives.html ../../Community/initiatives.md -H head.html -B nav.html
 pandoc -s -o ../../Catalog/library_criteria.html ../../Catalog/library_criteria.md -H head.html -B nav.html
