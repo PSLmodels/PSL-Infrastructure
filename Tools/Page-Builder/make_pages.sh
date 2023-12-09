8645a68d4af19e39a4da16028e4766a29fe47db2
fox8090 <matthew-fox@hotmail.co.uk>
2023-12-09 18:43:12 +0000

    Tidy up

--- Tools/Page-Builder/make_pages.sh
+++ Tools/Page-Builder/make_pages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-pandoc -s -o ../../Community/contribute.html ../../Community/contribute.md -H head.html -B nav.html -A footer.html --css ../../CSS/page.css
-pandoc -s -o ../../Community/council.html ../../Community/council.md -H head.html -B nav.html -A footer.html --css ../../CSS/page.css
-pandoc -s -o ../../Community/roadmap.html ../../Community/roadmap.md -H head.html -B nav.html -A footer.html --css ../../CSS/page.css
-pandoc -s -o ../../Community/initiatives.html ../../Community/initiatives.md -H head.html -B nav.html -A footer.html --css ../../CSS/page.css
-pandoc -s -o ../../Catalog/library_criteria.html ../../Catalog/library_criteria.md -H head.html -B nav.html -A footer.html --css ../../CSS/page.css
+pandoc --template=template.html -o ../../Community/contribute.html ../../Community/contribute.md
+pandoc --template=template.html -o ../../Community/council.html ../../Community/council.md
+pandoc --template=template.html -o ../../Community/roadmap.html ../../Community/roadmap.md
+pandoc --template=template.html -o ../../Community/initiatives.html ../../Community/initiatives.md
+pandoc --template=template.html -o ../../Catalog/library_criteria.html ../../Catalog/library_criteria.md
